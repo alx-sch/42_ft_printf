934ace00b52cb76b420b1ee15a38092657d8cf4b
Alexander Schenk <alexander.o.schenk@gmail.com>
2024-01-25 19:26:37 +0100

    all tested and good to go

--- Makefile
+++ Makefile
@@ -5,90 +5,43 @@
 #                                                     +:+ +:+         +:+      #
 #    By: aschenk <aschenk@student.42berlin.de>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2023/11/13 11:41:06 by aschenk           #+#    #+#              #
-#    Updated: 2024/01/20 20:58:29 by aschenk          ###   ########.fr        #
+#    Created: 2024/01/25 15:16:35 by aschenk           #+#    #+#              #
+#    Updated: 2024/01/25 19:25:57 by aschenk          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-NAME =			libft.a
+NAME =			libftprintf.a
 CC =			cc
 CFLAGS =		-Wall -Wextra -Werror
-
-# Macros
-## FOR GET_NEXT_LINE()
-BUFFER_SIZE ?= 42
-FD_SIZE ?= 1024
-
-SRCS =			ft_isalpha.c \
-				ft_isdigit.c \
-				ft_isalnum.c \
-				ft_isascii.c \
-				ft_isprint.c \
-				ft_strlen.c \
-				ft_toupper.c \
-				ft_tolower.c \
-				ft_strncmp.c \
-				ft_strlcpy.c \
-				ft_strlcat.c \
-				ft_strchr.c \
-				ft_strrchr.c \
-				ft_strnstr.c \
-				ft_memset.c \
-				ft_memchr.c \
-				ft_memcpy.c \
-				ft_memcmp.c \
-				ft_memmove.c \
-				ft_bzero.c \
-				ft_atoi.c \
-				ft_strdup.c \
-				ft_calloc.c \
-				ft_substr.c \
-				ft_strjoin.c \
-				ft_strtrim.c \
-				ft_split.c \
-				ft_itoa.c \
-				ft_strmapi.c \
-				ft_striteri.c \
-				ft_putchar_fd.c \
-				ft_putstr_fd.c \
-				ft_putendl_fd.c \
-				ft_putnbr_fd.c \
-				ft_lstnew.c \
-				ft_lstadd_front.c \
-				ft_lstsize.c \
-				ft_lstlast.c \
-				ft_lstadd_back.c \
-				ft_lstdelone.c \
-				ft_lstclear.c \
-				ft_lstiter.c \
-				ft_lstmap.c \
-				ft_isbinary.c \
-				get_next_line_bonus.c \
-
+SRCS =			ft_printf_utils.c ft_printf.c
 OBJS =			${SRCS:.c=.o}
 
 # Target 'all' is the default target, building library specified by $(NAME).
 all:	$(NAME)
 
-# Target $(NAME) depends on object files $(OBJS),
-# and the 'ar rc' command creates the library.
+# Target $(NAME) depends on object files $(OBJS).
+# Builds the 'libft' library and its respective .o files
+# (using '-C libft' to change the directory to 'libft' before executing 'make').
+# Creates libftprintf.a using the .o files in $(OBJS) and in the libft folder.
 $(NAME):	$(OBJS)
-	ar rcs $(NAME) $(OBJS)
+	make -C libft
+	ar rcs $(NAME) $(OBJS) libft/*.o
 
-# Target 'clean' removes object files.
+# Rule to define how to generate object files (%.o) from corresponding
+# source files (%.c). Each .o file depends on the associated .c file and the
+# libft library header file (libft/libft.h)
+# EXPLAIN FLAGS
+$(OBJS):	%.o: %.c libft/libft.h
+	$(CC) $(CFLAGS) -c -o $@ $<
+
 clean:
+	make -C libft clean
 	rm -f $(OBJS)
 
-# Target 'fclean' depends on 'clean' and removes the library as well.
 fclean:	clean
+	make -C libft clean
 	rm -f $(NAME)
 
-# Target 're' depends on 'fclean' and 'all', rebuilding the project from scratch.
 re:	fclean all
 
-# Rule to compile .c files to .o files with specified macros.
-%.o: %.c
-	$(CC) $(CFLAGS) -D BUFFER_SIZE=$(BUFFER_SIZE) -D FD_SIZE=$(FD_SIZE) -c $< -o $@
-
-# Marking rules as 'phony' to ensure that 'make' doesn't misinterpret them as files.
 .PHONY: all clean fclean re
---
+++ ft_printf.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aschenk <aschenk@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 23:43:10 by aschenk           #+#    #+#             */
+/*   Updated: 2024/01/25 19:08:05 by aschenk          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include "ft_printf.h"
+
+// Util Functions as found in ft_printf_utils.c
+int	print_count_char(char _char);
+int	print_count_string(char *str);
+int	print_count_unsigned(unsigned int nbr);
+int	print_count_number(int nbr);
+int	print_count_hex(uintptr_t nbr, char format);
+
+//	%p: Prints the memory address represented by a pointer,
+//	including the '0x' prefix to indicate hexadecimal (base 16) notation.
+//	Returns the printout length or '(nil)' if the pointer is NULL.
+static int	print_count_pointer(uintptr_t ptr)
+{
+	if (!ptr)
+		return (print_count_string("(nil)"));
+	else
+		return (print_count_string("0x") + print_count_hex(ptr, 'x'));
+}
+
+/*
+Processes and prints formatted data based on the given format specifier.
+
+@format:	A char representing the format specifier to determine the
+			type of data to print.
+@args:		A va_list containing the variable arguments
+			-> arguments after format string in ft_printf function call.
+
+Returns:	The total printout length of the formatted data
+			or -1 in case of an unsupported format specifier.
+*/
+static int	print_count_format(const char format, va_list args)
+{
+	int		len_format;
+
+	len_format = 0;
+	if (format == 'c')
+		len_format = print_count_char(va_arg(args, int));
+	else if (format == 's')
+		len_format = print_count_string(va_arg(args, char *));
+	else if (format == 'p')
+		len_format = print_count_pointer((uintptr_t)va_arg(args, void *));
+	else if (format == 'd' || format == 'i')
+		len_format = print_count_number(va_arg(args, int));
+	else if (format == 'u')
+		len_format = print_count_unsigned(va_arg(args, unsigned int));
+	else if (format == 'x' || format == 'X')
+		len_format = print_count_hex(va_arg(args, unsigned int), format);
+	else if (format == '%')
+		len_format = print_count_char('%');
+	else
+		return (-1);
+	return (len_format);
+}
+
+/*
+A simplified implementation of the printf function in C.
+
+@format:	A format string containing placeholders for various data types.
+			Supported format specifiers: 'c', 's', 'p', 'd', 'i', 'u', 'x', 'X', '%'.
+@...:		Variable arguments to be formatted according to the format string.
+
+Returns:	The total length of characters printed.
+
+Iterates through the format string, replacing placeholders with
+actual values from the variable arguments.
+*/
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		i;
+	int		total_len;
+
+	i = 0;
+	total_len = 0;
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%' && ft_strchr("cspdiuxX%", format[i + 1]) != 0)
+		{
+			total_len += print_count_format(format[i + 1], args);
+			i++;
+		}
+		else
+			total_len += print_count_char(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (total_len);
+}

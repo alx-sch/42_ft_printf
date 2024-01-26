e8d80b5734cc83eb6c40ff190579dad902799d7f
Alexander Schenk <alexander.o.schenk@gmail.com>
2024-01-26 11:19:00 +0100

    tested testing file

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: aschenk <aschenk@student.42berlin.de>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/01/25 15:16:35 by aschenk           #+#    #+#              #
-#    Updated: 2024/01/25 19:25:57 by aschenk          ###   ########.fr        #
+#    Updated: 2024/01/26 11:12:30 by aschenk          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,18 +19,20 @@ OBJS =			${SRCS:.c=.o}
 # Target 'all' is the default target, building library specified by $(NAME).
 all:	$(NAME)
 
-# Target $(NAME) depends on object files $(OBJS).
+# Target $(NAME) depends on object files $(OBJS) and libft library header.
 # Builds the 'libft' library and its respective .o files
-# (using '-C libft' to change the directory to 'libft' before executing 'make').
+# (using '-C libft' to change the directory to 'libft', before executing 'make').
 # Creates libftprintf.a using the .o files in $(OBJS) and in the libft folder.
-$(NAME):	$(OBJS)
+$(NAME):	$(OBJS) libft/libft.h
 	make -C libft
 	ar rcs $(NAME) $(OBJS) libft/*.o
 
 # Rule to define how to generate object files (%.o) from corresponding
 # source files (%.c). Each .o file depends on the associated .c file and the
 # libft library header file (libft/libft.h)
-# EXPLAIN FLAGS
+# -c:		Generates o. files without linking.
+# -o $@:	Output file name;  '$@' is replaced with target name (the o. file).
+# -$<:		Represents the first prerequisite (the c. file).
 $(OBJS):	%.o: %.c libft/libft.h
 	$(CC) $(CFLAGS) -c -o $@ $<
 
@@ -39,7 +41,7 @@ clean:
 	rm -f $(OBJS)
 
 fclean:	clean
-	make -C libft clean
+	make -C libft fclean
 	rm -f $(NAME)
 
 re:	fclean all
---
+++ test_ft_printf.c
@@ -0,0 +1,174 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_printf.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aschenk <aschenk@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/26 11:13:24 by aschenk           #+#    #+#             */
+/*   Updated: 2024/01/26 11:17:57 by aschenk          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+When located in the root directory, compile the test program:
+-	'make' to generate libftprintf.a
+-	(optional: 'make clean' for cleanup)
+-	'cc -o test test_ft_printf.c libftprintf.a' to compile the test program
+-	'./test' to run it.
+*/
+
+#include <stdio.h>
+#include "ft_printf.h"
+
+int	main(void)
+{
+	int		length_my_fct;
+	int		length_native;
+	char	*ptr;
+	char	str[] = "Hello World!!";
+	int		nbr;
+	int		neg_nbr;
+
+	ptr = str;
+	nbr = 42133742;
+	neg_nbr = -424242;
+
+	// Testing w/o any format specificer
+	printf("\n== TESTING: No Format Specificer ==\n");
+	printf("-- ft_printf(): --\n");
+	length_my_fct = ft_printf("No formatters be found!\n"); ////////
+	printf("Return value: %d\n", length_my_fct);
+	length_native = printf("No formatters to be found!\n"); ////////
+	printf("Return value: %d\n", length_native);
+
+	// Testing empty string
+	printf("\n== TESTING: printf('') ==\n");
+	printf("-- ft_printf(): --\n");
+	length_my_fct = ft_printf(""); ////////
+	printf("\nReturn value: %d\n", length_my_fct);
+
+	printf("-- printf(): --\n");
+	length_native = printf(""); ////////
+	printf("\nReturn value: %d\n", length_native);
+	printf("\n============\n\n");
+
+	// Testing %c
+	printf("== TESTING: %%c ==\n");
+	printf("-- ft_printf(): --\n");
+	length_my_fct = ft_printf("Printing a char: %c\n", 'z'); ////////
+	printf("Return value: %d\n", length_my_fct);
+
+	printf("-- printf(): --\n");
+	length_native = printf("Printing a char: %c\n", 'z'); ////////
+	printf("Return value: %d\n", length_native);
+	printf("\n============\n\n");
+
+	// Testing %%
+	printf("== TESTING: %%%% ==\n");
+	printf("-- ft_printf(): --\n");
+	length_my_fct = ft_printf("This project is 100%% correct!\n"); ////////
+	printf("Return value: %d\n", length_my_fct);
+
+	printf("-- printf(): --\n");
+	length_native = printf("This project is 100%% correct!\n"); ////////
+	printf("Return value: %d\n", length_native);
+	printf("\n============\n\n");
+
+	// Testing %s
+	printf("== TESTING: %%s ==\n");
+	printf("-- ft_printf(): --\n");
+	length_my_fct = ft_printf("Printing a string: %s\n", str); ////////
+	printf("Return value: %d\n", length_my_fct);
+
+	printf("-- printf(): --\n");
+	length_native = printf("Printing a string: %s\n", str); ////////
+	printf("Return value: %d\n", length_native);
+	printf("\n============\n\n");
+
+	// Testing %s NULL string
+	printf("== TESTING: %%s NULL string ==\n");
+	printf("-- ft_printf(): --\n");
+	length_my_fct = ft_printf("Printing a string: %s\n", (char *)NULL); ////////
+	printf("Return value: %d\n", length_my_fct);
+
+	printf("-- printf(): --\n");
+	length_native = printf("Printing a string: %s\n", (char *)NULL); ////////
+	printf("Return value: %d\n", length_native);
+	printf("\n============\n\n");
+
+	// Testing %d
+	printf("== TESTING: %%d ==\n");
+	printf("-- ft_printf(): --\n");
+	length_my_fct = ft_printf("Printing a decimal number: %d\n", nbr); ////////
+	printf("Return value: %d\n", length_my_fct);
+
+	printf("-- printf(): --\n");
+	length_native = printf("Printing a decimal number: %d\n", nbr); ////////
+	printf("Return value: %d\n", length_native);
+	printf("\n============\n\n");
+
+	// Testing %i
+	printf("== TESTING: %%i ==\n");
+	printf("-- ft_printf(): --\n");
+	length_my_fct = ft_printf("Printing an integer base 10 "
+		"(same as decimal): %i\n", nbr); ////////
+	printf("Return value: %d\n", length_my_fct);
+
+	printf("-- printf(): --\n");
+	length_native = printf("Printing an integer base 10 "
+		"(same as decimal): %i\n", nbr); ////////
+	printf("Return value: %d\n", length_native);
+	printf("\n============\n\n");
+
+	// Testing %u
+	printf("== TESTING: %%u ==\n");
+	printf("-- ft_printf(): --\n");
+	length_my_fct = ft_printf("Printing %d as unsinged: %u\n", ////
+		neg_nbr, neg_nbr); ////////
+	printf("Return value: %d\n", length_my_fct);
+
+	printf("-- printf(): --\n");
+	length_native = printf("Printing %d as unsinged: %u\n", ////
+		neg_nbr, neg_nbr); ////////
+	printf("Return value: %d\n", length_native);
+	printf("\n============\n\n");
+
+	// Testing %p
+	printf("== TESTING: %%p ==\n");
+	printf("-- ft_printf(): --\n");
+	length_my_fct = ft_printf("Printing pointer: %p\n", (void *)ptr); ////////
+	printf("Return value: %d\n", length_my_fct);
+
+	printf("-- printf(): --\n");
+	length_native = printf("Printing pointer: %p\n", (void *)ptr); ////////
+	printf("Return value: %d\n", length_native);
+	printf("\n============\n\n");
+
+	// Testing %p null pointer
+	ptr = NULL;
+	printf("== TESTING: %%p (null pointer)==\n");
+	printf("-- ft_printf(): --\n");
+	length_my_fct = ft_printf("Printing pointer: %p\n", (void*)ptr); ////////
+	printf("Return value: %d\n", length_my_fct);
+
+	printf("-- printf(): --\n");
+	length_native = printf("Printing pointer: %p\n", (void*)ptr); ////////
+	printf("Return value: %d\n", length_native);
+	printf("\n============\n\n");
+
+		// Testing %x %X
+	printf("== TESTING: %%x/%%X ==\n");
+	printf("-- ft_printf(): --\n");
+	length_my_fct = ft_printf("Printing %d as hex: %x\n", nbr, nbr); ////////
+	ft_printf("Printing %d as HEX: %X\n", nbr, nbr); ////////
+	printf("Return value: %d\n", length_my_fct);
+
+	printf("-- printf(): --\n");
+	length_native = printf("Printing %d as hex: %x\n", nbr, nbr); ////////
+	printf("Printing %d as HEX: %X\n", nbr, nbr); ////////
+	printf("Return value: %d\n", length_native);
+	printf("\n============\n\n");
+
+	return (0);
+}
